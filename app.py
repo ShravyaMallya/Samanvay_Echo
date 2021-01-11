d8d09759cc08aeea30ef743c7a4b7535cbe533cd
ShravyaMallya <mallyashravya@gmail.com>
2021-01-11 22:38:29 +0530

    added 3dmodel.html

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def index():
 def homePage():
     return render_template('home.html')
 
+@app.route('/3dmodel')
+def model():
+    return render_template('3dmodel.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
