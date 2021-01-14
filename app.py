1aa42e62c227a57f10930091c370c3eeea05d40b
ShravyaMallya <mallyashravya@gmail.com>
2021-01-14 21:49:48 +0530

    Updated contact page

--- app.py
+++ app.py
@@ -21,6 +21,10 @@ def model():
 def learn():
     return render_template('learn.html')
 
+@app.route('/contact')
+def contact():
+   return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
